9332de3aaa67b767945382f35caa8e4b46aae6c3
Frank Fang <frankfang1990@gmail.com>
2014-07-28 15:27:10 +0800

    do not support stream

--- index.js
+++ index.js
@@ -5,125 +5,46 @@
 var fs = require('fs')
 var path = require('path')
 var through = require('through2')
-var gutil = require('gulp-util')
-var PluginError = gutil.PluginError
+var gUtil = require('gulp-util')
+var PluginError = gUtil.PluginError
 var es = require('event-stream')
 
 var pkg = require('./package.json')
 
-function makeStream() {
-    var stream = through(function () {
-        console.log("Object.keys(this):")
-        console.log(Object.keys(this))
-        this.queue('1')
-    });
-    return stream;
-}
 
 function plugin(options) {
     options = options || {}
 
     var prefix = new Buffer('hi ')
 
-    var stream = through.obj(function (file, enc, cb) {
+    var extender = through.obj(function (file, enc, cb) {
 
         if (file.isNull()) {
-//            console.log('file is null')
+            this.push(file)
+            return cb()
         }
 
         if (file.isBuffer()) {
-            console.log('file is buffer')
 
             extendFile(file, function () {
                 this.push(file)
-                cb()
+                return cb()
             }.bind(this))
-
-
         }
 
         if (file.isStream()) {
-            console.log('file is stream')
-            var streamer = makeStream();
-            streamer.on('error', this.emit.bind(this, 'error'));
-
-            var masterFile
-
-            file.contents = file.contents
-                .pipe(es.split())
-                .pipe(through.obj(function (chunk, enc, next) {
-                    var line = chunk.toString()
-                    if (!masterFile) {
-                        var masterRelativePath = findMaster(line)
-                        masterFile = makeFileSync(path.join(file.base, masterRelativePath))
-                    } else {
-                        this.push('hi \n')
-                        next()
-                        return
-
-//                        var masterAbsolute = path.join(file.base, masterRelativePath)
-//                        makeFile(masterAbsolute, function (masterFile) {
-//
-//                            extendFile(masterFile, function () {
-//
-//                                var masterContent = masterFile.contents.toString()
-//                                var lines = masterContent.split(/\n|\r|\r\n/)
-//
-//                                var newLines = lines.map(function (line, index, array) {
-//                                    var blockName = findPlaceholder(line)
-//                                    if (blockName) {
-//                                        var blockContent = getBlock(file.contents.toString(), blockName)
-//                                        return blockContent || line
-//                                    } else {
-//                                        return line
-//                                    }
-//                                })
-//
-//                                var newContent = newLines.join('\n')
-//
-//                                this.push(newContent)
-//                                next()
-//                                return
-//
-//
-//                            }.bind(this))
-//
-//                        }.bind(this))
-                    }
-
-                    this.push(chunk + '\n')
-                    next()
-                }))
-                .on('end', function () {
-                    cb()
-                })
-            this.push(file)
-
-//            var masterAbsolute = path.join(file.base, masterRelativePath)
-//            var masterReader = fs.createReadStream(masterAbsolute)
-//            var content = new Buffer('')
-//            masterReader.pipe(es.split())
-//                .pipe(es.map(function (data, cb) {
-//                    var placeholder = findPlaceholder(data.toString())
-//                    cb(null, placeholder ? placeholder : data)
-//                }))
-//                .on('data', function (data) {
-//                    console.log(data)
-//
-//                })
-//
-//            file.contents = Buffer.concat([prefix, file.contents]);
+            return new PluginError(pkg.name, 'Streaming is not supported')
         }
 
     })
 
-    return stream
+    return extender
 }
 
 function makeFile(absolutePath, cb) {
     fs.readFile(absolutePath, function (error, data) {
         if (error) { throw error }
-        var file = new gutil.File({
+        var file = new gUtil.File({
             base: path.dirname(absolutePath),
             path: absolutePath,
             contents: new Buffer(data),
@@ -134,7 +55,7 @@ function makeFile(absolutePath, cb) {
 
 function makeFileSync(absolutePath) {
     var contents = fs.readFileSync(absolutePath, {flag: 'r'})
-    var file = new gutil.File({
+    var file = new gUtil.File({
         base: path.dirname(absolutePath),
         path: absolutePath,
         contents: new Buffer(contents),
